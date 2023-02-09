6dd9cc120d572a79f769a792a68a4d15731ea2fb
denes16 <74267424+denes16@users.noreply.github.com>
2023-02-08 22:23:24 -0600

    feat(auth): use google id token

--- src/features/auth/strategies/google-access.strategy.ts
+++ src/features/auth/strategies/google-access.strategy.ts
@@ -1,15 +1,18 @@
 import { PassportStrategy } from '@nestjs/passport';
-import { Injectable } from '@nestjs/common';
+import { Injectable, ConflictException } from '@nestjs/common';
 import { AuthProvider, User } from '@prisma/client';
 import { CurrentUser } from '../types/current-user.type';
 import { CaslAbilityFactoryService } from '../casl-ability-factory.service';
 import { accessibleBy } from '@casl/prisma';
 import { ConfigService } from '@nestjs/config';
-import { Strategy } from 'passport-google-oauth20';
+import * as Strategy from 'passport-google-id-token';
 import { PrismaService } from '../../../core/services/prisma/prisma.service';
 
 @Injectable()
-export class GoogleAccessStrategy extends PassportStrategy(Strategy, 'google') {
+export class GoogleAccessTokenStrategy extends PassportStrategy(
+  Strategy,
+  'google-token',
+) {
   constructor(
     private readonly caslAbilityFactoryService: CaslAbilityFactoryService,
     private configService: ConfigService,
@@ -32,15 +35,12 @@ export class GoogleAccessStrategy extends PassportStrategy(Strategy, 'google') {
       session: false,
     };
   }
-  async validate(
-    accessToken: string,
-    refreshToken: string,
-    profile: any,
-  ): Promise<CurrentUser> {
-    const userId = profile.id;
-    const email = profile.emails[0].value;
-    const firstName = profile.name.givenName;
-    const lastName = profile.name.familyName ?? '';
+  async validate(parsedToken: any, googleId: string): Promise<CurrentUser> {
+    const profile = parsedToken.payload;
+    const userId = googleId;
+    const email = profile.email;
+    const firstName = profile.name ?? '';
+    const lastName = profile.family_name ?? '';
     let user = await this.prismaService.user.findFirst({
       where: {
         authProvider: AuthProvider.GOOGLE,
@@ -48,6 +48,14 @@ export class GoogleAccessStrategy extends PassportStrategy(Strategy, 'google') {
       },
     });
     if (!user) {
+      const existingUser = await this.prismaService.user.findFirst({
+        where: {
+          email,
+        },
+      });
+      if (existingUser) {
+        throw new ConflictException('errors.emailAlreadyExists');
+      }
       user = await this.prismaService.user.create({
         data: {
           email,
@@ -65,8 +73,8 @@ export class GoogleAccessStrategy extends PassportStrategy(Strategy, 'google') {
       id: user.id,
       user: user,
       ability,
-      authAccessToken: accessToken,
-      authRefreshToken: refreshToken,
+      authAccessToken: '',
+      authRefreshToken: '',
       getAccessibleWhereInput(subject, action?) {
         const subjectName =
           typeof subject === 'string' ? subject : subject.modelName;
